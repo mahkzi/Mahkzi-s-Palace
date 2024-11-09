4176274cf90e88492268eeb9c39d0549a1f20d16
Maximo Castro <maximocastrobraga@gmail.com>
2024-11-09 15:48:27 -0300

    listo el header del index

--- js/script.js
+++ js/script.js
@@ -8,12 +8,28 @@ nav.appendChild(links);
 const ulista =document.createElement("ul");
 ulista.className="contenedor-li";
 links.appendChild(ulista);
+const lia=document.createElement("li");
+lia.className="lista";
+ulista.appendChild(lia);
+const lib=document.createElement("li");
+lib.className="lista";
+ulista.appendChild(lib);
+const inicio=document.createElement("a");
+inicio.href="./index.html";
+inicio.innerText="Inicio";
+inicio.className="links"
+lia.appendChild(inicio);
+const nosotros=document.createElement("a");
+nosotros.href="/pages/nosotros.html"
+nosotros.innerText="Sobre nosotros";
+nosotros.className="links"
+lib.appendChild(nosotros);
 const icono=document.createElement("div")
 icono.className="contenedor-icono";
 nav.appendChild(icono);
 const imagen=document.createElement("img");
-imagen.className= "img";
-imagen.src="../assets/Eames-blanco-600x629-1-e1693398098543.jpg";
+imagen.src="./assets/icons8-carrito-de-compras-50.png";
 imagen.alt="icono del carrito";
+imagen.className="img";
 icono.appendChild(imagen);
 console.log(header);
