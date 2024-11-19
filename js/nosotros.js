83ae2e813d2ed9f14417c9757a65a927b34fa3be
Maximo Castro <maximocastrobraga@gmail.com>
2024-11-19 13:42:09 -0300

    cambie del js al html

--- js/nosotros.js
+++ js/nosotros.js
@@ -1,19 +1,3 @@
-const header=document.getElementById("contenedor-header");
-const nav=document.createElement("nav");
-nav.className="nav-bar"
-header.appendChild(nav);
-const ul=document.createElement("ul");
-ul.className="ulista";
-nav.appendChild(ul);
-const li=document.createElement("li");
-li.className="lista";
-ul.appendChild(li);
-const link=document.createElement("a");
-link.href="../index.html";
-link.innerText="Volver al inicio";
-link.className="link";
-li.appendChild(link);
-console.log(header);
 const section=document.getElementById("contenedor-section");
 const textoSection=document.createElement("div");
 textoSection.className="info"
@@ -36,31 +20,6 @@ boton.addEventListener("click",()=> enviarDatos());
 formulario.appendChild(boton);
 console.log(section);
 /*footer*/
-const footer=document.getElementById("footer-container");
-const containerFooter=document.createElement("div");
-containerFooter.className="contenedor-footer";
-footer.appendChild(containerFooter);
-const contenedorTexto=document.createElement("div");
-contenedorTexto.className="texto-container";
-containerFooter.appendChild(contenedorTexto);
-const textoFooter=document.createElement("h3");
-textoFooter.className="texto-footer";
-textoFooter.innerText="Síguenos en nuestras redes"
-const containerImg=document.createElement("div");
-containerImg.className="footer-imagenes";
-containerFooter.appendChild(containerImg);
-const imagenIg=document.createElement("img");
-imagenIg.src="../assets/icons8-instagram-48.png";
-imagenIg.alt="se ha producido un error";
-imagenIg.className="imagen-footer";
-const imagenFacebook=document.createElement("img");
-imagenFacebook.src="../assets/icons8-facebook-nuevo-48.png";
-imagenFacebook.alt="se ha producido un error";
-imagenFacebook.className="imagen-footer";
-contenedorTexto.appendChild(textoFooter);
-containerImg.appendChild(imagenIg);
-containerImg.appendChild(imagenFacebook);
-console.log(footer); 
 function enviarDatos(){
     Swal.fire({
         title: "!Muchas Gracias!",
--- js/script.js
+++ js/script.js
@@ -1,40 +1,4 @@
-const body=document.getElementById("gran-contenedor");
-const header = document.getElementById("contenedor");
-const nav = document.createElement("nav");
-nav.className= "navegador";
-header.appendChild(nav);
-const links=document.createElement("div")
-links.className="contenedor-ul";
-nav.appendChild(links);
-const ulista =document.createElement("ul");
-ulista.className="contenedor-li";
-links.appendChild(ulista);
-const lia=document.createElement("li");
-lia.className="lista";
-ulista.appendChild(lia);
-const lib=document.createElement("li");
-lib.className="lista";
-ulista.appendChild(lib);
-const inicio=document.createElement("a");
-inicio.href="./index.html";
-inicio.innerText="Inicio";
-inicio.className="links"
-lia.appendChild(inicio);
-const nosotros=document.createElement("a");
-nosotros.href="/pages/nosotros.html"
-nosotros.innerText="Sobre nosotros";
-nosotros.className="links"
-lib.appendChild(nosotros);
-const icono=document.createElement("div")
-icono.className="contenedor-icono";
-nav.appendChild(icono);
-const imagen=document.createElement("img");
-imagen.src="./assets/icons8-carrito-de-compras-50.png";
-imagen.alt="icono del carrito";
-imagen.className="img";
-imagen.addEventListener("click",()=>mostrarCarrito());
-icono.appendChild(imagen);
-console.log(header);
+
 function mostrarCarrito(){
   const carritoDiv=document.createElement("div");
   carritoDiv.className="container-carrito";
@@ -140,29 +104,3 @@ function agregarAlCarrito(producto){
         }
     localStorage.setItem("carrito", JSON.stringify(carrito));
 };
-//footer
-const footer=document.getElementById("footer-container");
-const containerFooter=document.createElement("div");
-containerFooter.className="contenedor-footer";
-footer.appendChild(containerFooter);
-const contenedorTexto=document.createElement("div");
-contenedorTexto.className="texto-container";
-containerFooter.appendChild(contenedorTexto);
-const textoFooter=document.createElement("h3");
-textoFooter.className="texto-footer";
-textoFooter.innerText="Síguenos en nuestras redes"
-const containerImg=document.createElement("div");
-containerImg.className="footer-imagenes";
-containerFooter.appendChild(containerImg);
-const imagenIg=document.createElement("img");
-imagenIg.src="../assets/icons8-instagram-48.png";
-imagenIg.alt="se ha producido un error";
-imagenIg.className="imagen-footer";
-const imagenFacebook=document.createElement("img");
-imagenFacebook.src="../assets/icons8-facebook-nuevo-48.png";
-imagenFacebook.alt="se ha producido un error";
-imagenFacebook.className="imagen-footer";
-contenedorTexto.appendChild(textoFooter);
-containerImg.appendChild(imagenIg);
-containerImg.appendChild(imagenFacebook);
-console.log(footer);
